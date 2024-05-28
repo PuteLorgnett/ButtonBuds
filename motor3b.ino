3274467549ed3a9c05b78680d9d741929f345157
PuteLorgnett <162039451+PuteLorgnett@users.noreply.github.com>
2024-05-28 14:10:00 +0200

    Forslag motor3b.ino

--- motor3b.ino
+++ motor3b.ino
@@ -1,126 +1,97 @@
-#include <Stepper.h>              //brukes med trinnmotoren og driverkort
-const int stepsPerRevolution = 64;  // number of steps per revolution, brukes med trinnmotoren og driverkort
-Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11); //brukes for driverkort
+#include <Stepper.h>			// bibliotek for trinnmotoren og driverkort
+const int trinnPerRotasjon = 64;	// antall ”trinn” per motor-rotasjon, brukes med trinnmotoren og driverkort
+Stepper kamMotor(trinnPerRotasjon, 8, 10, 9, 11); // pin-nr for motoren, brukes for driverkort
 
-// Hva det er og hvilken pin den bruker. (pedal er knapp)
+// pin-nr for pedal-knappen
 int pedalKnapp = 3;
 
-// ettSteg er hvor mange steps*64 som trengs for å flytte kammen fra tann til tann
-// halvtSteg er da 11*64 steps for å flytte en halv tann på kammen
-// disse variablene brukes bare i deres funksjoner
+// ettSteg er hvor mange motor-rotasjoner som trengs for å flytte kammen fra ”tann til tann”
+// halvtSteg er halvparten antall rotasjoner
 int ettSteg = 22;
 int halvtSteg = 11;
 
-int period = 1000;          //tidsinterval for hvor lenge den skal vente på å registrere knappe trykk. brukes med millis()
-unsigned long time_now = 0; //blir brukt med millis()
+int periode = 1000;	// Variabelen periode er reaksjonstiden til pedalen. Dermed bestemmer den også hvor raskt et dobbelttrykk må være. Brukes med millis()
+unsigned long tidTeller = 0;	// Blir brukt med millis()
 
-int holdeInneTeller = 0; //Denne blir å enten være 0 eller langt over 0
-int knappTeller = 0; // Denne teller hvor mange ganger holdeInneTeller går over 0
+int holdeInneTeller = 0; 	// Denne blir å enten være 0 eller langt over 0
+int antallTrykkTeller = 0; 	// Denne teller hvor mange ganger holdeInneTeller går over 0
 
-
-
-
-// tall = 16 er er halv rotasjon hos motorer
-// tall = 8 er en kvart
+// tall er enten ettSteg eller halvtSteg
 void counterclockwise(int tall) {
-  Serial.println("clockwise");
-  myStepper.step(stepsPerRevolution*tall);
+	kamMotor.step(trinnPerRotasjon*tall);
 }
 
-// tall = 16 er er halv rotasjon hos motoren
-// tall = 8 er en kvart
 void clockwise(int tall) {
-  Serial.println("counterclockwise");
-  myStepper.step(-stepsPerRevolution*tall);
+	kamMotor.step(-trinnPerRotasjon*tall);
 }
 
-// Kjører ett hakk på kammen. retning velger rotasjons retning
-void etSteg (String retning) {
-  Serial.println("Kjører ett steg på kammen");
-  if (retning == "+") {
-    clockwise(ettSteg);
-  }
-  else if (retning == "-") {
-    counterclockwise(ettSteg);
-  }
+// Kjører ett hakk på kammen. retning bestemmer rotasjonsretning
+void helSteg(String retning) {
+	if (retning == ”+”) {
+		clockwise(ettSteg);
+	}
+	else if (retning == ”-”) {
+		counterclockwise(ettSteg);
+	}
 }
 
-// Kjører halvt hakk på kammen. retning velger rotasjons retning
+// Kjører et halvt hakk på kammen. retning bestemmer rotasjonsretning
 void halvSteg (String retning) {
-  Serial.println("Kjører ett steg på kammen");
-  if (retning == "+") {
-    clockwise(halvtSteg);
-  }
-  else if (retning == "-") {
-    counterclockwise(halvtSteg);
-  }
+	if (retning == ”+”) {
+		clockwise(halvtSteg);
+	}
+	else if (retning == ”-”) {
+		counterclockwise(halvtSteg);
+	}
 }
 
 
-// blar et ark. retning velger rotasjons retning
+
+// Blar et ark. retning bestemmer rotasjonsretning
 void blaArk(String retning) {
-  if (retning == "+") {
-    etSteg("+");
-    halvSteg("+");
-    halvSteg("-");;
-  }
-  else if (retning == "-") {
-    etSteg("-");
-    halvSteg("-");
-    halvSteg("+");
-  }
+	if (retning == ”+”) {
+		helSteg(”+”);
+		halvSteg(”+”);
+		halvSteg(”-”);;
+	}
+	else if (retning == ”-”) {
+		helSteg(”-”);
+		halvSteg(”-”);
+		halvSteg(”+”);
+	}
 }
-
-
 
 void setup() {
-  pinMode(pedalKnapp,INPUT_PULLUP);
-
-  myStepper.setSpeed(400); // hastighet på motoren
-  Serial.begin(9600);
+	pinMode(pedalKnapp, INPUT_PULLUP);
+	kamMotor.setSpeed(400); // hastighet på motoren
+	Serial.begin(9600);
 }
 
-
-
 void loop() {
+// holdeInneTeller sin verdi stiger så lenge knappen(pedalen) holdes inne. Programmet er skrevet slik at et langt trykk oppfattes som kun ett trykk
+	if (digitalRead(pedalKnapp) == LOW) {
+		holdeInneTeller += 1;
+	}
 
-  //knappen summer holdeInneTeller når den er trykket inn
-  if (digitalRead(pedalKnapp) == LOW) {
-    holdeInneTeller += 1;
-  }
+// når knappen blir sluppet teller antallTrykkTeller antall trykk. Funksjonen setter også tidTeller for millis()-betingelsen i neste if-setning
+	else if (holdeInneTeller > 0) {
+		antallTrykkTeller += 1;
+		holdeInneTeller = 0;
+		tidTeller = millis();
+	}
+	
+	if (antallTrykkTeller > 0 && (millis() - tidTeller) > periode) {
 
-  // når knappen blir trykket inn så vil den "+=" for mange ganger i sekundet
-  // så trenger en til teller som heter knappTeller som konverterer fra holdeInneTeller
+		// Blar en side frem hvis antallTrykkTeller = 1
+		if (antallTrykkTeller == 1) {	
+			blaArk(”+”);
+		}
 
-  // når knappen blir sluppet, vil denne telle antall trykk
-  // den vil også sette variablene slik at millis() if-setningen under vil fungere
-  else if (holdeInneTeller > 0) {
-    knappTeller += 1;
-    holdeInneTeller = 0;
-    time_now = millis();
-  }
+		// Blar bakover hvis antallTrykkTeller = 2
+		else if (antallTrykkTeller == 2) { 
+			blaArk(”-”);
+		}
 
-  // får et schrodinger fenomen. trenger denne for at den skal kunne rotere andre veien.
-  Serial.println(knappTeller);
-  
-  // period velger hvor lenge etter knappe-/pedaltrykk den skal vente på å kjøre koden under
-  // valgte å lage dette fordi da er et trykk en side frem, og dobbel trykk en side tilbake 
-  if (knappTeller > 0 && millis() - time_now > period) {
-    
-    // Blar en side frem hvis knappteller = 1
-    if (knappTeller == 1) {
-      blaArk("+");
-    }
-
-    // Blar en side tilbake hvis knappteller = 2
-    else if (knappTeller == 2) {
-      blaArk("-");
-    }
-
-    //nullstiller
-    knappTeller = 0;
-      
-  }
-  
-
+		antallTrykkTeller = 0; // nullstiller
+	}
 }
